41f22238d451b74968736bc98110625d002d6cb4
mmaras1999 <mmaras1999@gmail.com>
2019-08-15 16:22:52 +0200

    Added Linear Sieve

---
+++ number_theory/linear_sieve.cpp
@@ -0,0 +1,23 @@
+namespace LinearSieve
+{
+    vector <bool> sieve;
+    vector <int> primes;
+
+    void init(int N)
+    {
+        ++N;
+        sieve.resize(N, 0);
+
+        FOR(i, 2, N) 
+        {
+            if (!sieve[i]) 
+            {
+                sieve[i] = i;
+                primes.PB(i);
+            }
+
+            for (int j = 0; j < SIZE(primes) && primes[j] <= sieve[i] && i * primes[j] <= N; ++j)
+                sieve[i * primes[j]] = primes[j];
+        }
+    }
+}
